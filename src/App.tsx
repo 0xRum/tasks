66536b656e1b3aed061f12612d050e12b75152db
0xR <114037840+0xRum@users.noreply.github.com>
2025-02-14 00:29:40 -0500

    Solved html.css tests

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Button, Container, Row, Col } from "react-bootstrap";
 import "./App.css";
 
 function App(): React.JSX.Element {
@@ -7,12 +8,57 @@ function App(): React.JSX.Element {
             <header className="App-header">
                 UM COS420 with React Hooks and TypeScript
             </header>
+            <h1>Heading 1</h1>
+            <img src="..assets/images/" alt="this is some alt text" />
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
             </p>
             <p>Garrett Rumery</p>
-            <p>Hello World</p>
+            This is <span style={{ color: "red" }}>colored text</span>.
+            <ol>
+                <li>first list</li>
+                <li>second list</li>
+                <li>third list</li>
+            </ol>
+            <Button
+                onClick={() => {
+                    console.log("Hello World!");
+                }}
+            >
+                Log Hello World
+            </Button>
+            <Container>
+                <Row>
+                    <Col>
+                        <div
+                            style={{
+                                width: "auto",
+                                height: "auto",
+                                backgroundColor: "red",
+                            }}
+                        >
+                            First column
+                        </div>
+                    </Col>
+                    <Col>
+                        <div
+                            style={{
+                                width: "auto",
+                                height: "auto",
+                                backgroundColor: "red",
+                            }}
+                        >
+                            Second Column. Lorem ipsum dolor, sit amet
+                            consectetur adipisicing elit. Harum eius
+                            reprehenderit quasi magnam, consequuntur hic
+                            distinctio nobis quam aliquid aperiam laborum culpa
+                            quia repellendus. Esse non suscipit ea repellendus
+                            assumenda.
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 }
