b5a11a0b48f2497832f9e469f598880d1fc2f688
0xR <114037840+0xRum@users.noreply.github.com>
2025-02-25 03:46:55 -0500

    Update arrays.ts

--- src/arrays.ts
+++ src/arrays.ts
@@ -106,5 +106,5 @@ export function makeMath(addends: number[]): string {
  * And the array [1, 9, 7] would become [1, 9, 7, 17]
  */
 export function injectPositive(values: number[]): number[] {
-    return values.map((value) => value + 1);
+    return [];
 }
