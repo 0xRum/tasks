f2ab487a995e8d5a589ae29635a078a84e78bce3
0xR <114037840+0xRum@users.noreply.github.com>
2025-02-25 02:55:07 -0500

    Update arrays.ts
    returns empty array
    returns single element array
    returns first and last with 2 or more arrays

--- src/arrays.ts
+++ src/arrays.ts
@@ -5,7 +5,15 @@
  * the number twice.
  */
 export function bookEndList(numbers: number[]): number[] {
-    return numbers;
+    if (numbers.length === 0) {
+        return [];
+    }
+    if (numbers.length === 1) {
+        return [...numbers, ...numbers];
+    }
+    return numbers.filter(
+        (_, index) => index === 0 || index === numbers.length - 1,
+    );
 }
 
 /**
