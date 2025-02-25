d144668550b601739eff979e7b4db32ee2c60c30
0xR <114037840+0xRum@users.noreply.github.com>
2025-02-25 03:11:05 -0500

    Update arrays.ts
    consumes array of strings and converts to ints.
    if number cant parse, convert to 0

--- src/arrays.ts
+++ src/arrays.ts
@@ -29,7 +29,10 @@ export function tripleNumbers(numbers: number[]): number[] {
  * the number cannot be parsed as an integer, convert it to 0 instead.
  */
 export function stringsToIntegers(numbers: string[]): number[] {
-    return [];
+    return numbers.map((num: string): number => {
+        const parsedNumber = parseInt(num);
+        return isNaN(parsedNumber) ? 0 : parsedNumber;
+    });
 }
 
 /**
