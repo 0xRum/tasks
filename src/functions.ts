f90dbee73a69055b8ab3f70e77dc0a46074dbd76
0xR <114037840+0xRum@users.noreply.github.com>
2025-02-24 22:02:11 -0500

    Update functions.ts
    returns true if yes YES or Yes
    returns false if no NO or No
    null elsewise

--- src/functions.ts
+++ src/functions.ts
@@ -41,5 +41,9 @@ export function isQuestion(message: string): boolean {
  * upper or lower case), then return `false`. Otherwise, return `null`.
  */
 export function convertYesNo(word: string): boolean | null {
-    return true;
+    return (
+        word.toLowerCase() === "yes" ? true
+        : word.toLowerCase() === "no" ? false
+        : null
+    );
 }
