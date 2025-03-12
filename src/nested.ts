957b62bd0d03d8f6b95b58f377379879d848d335
0xR <114037840+0xRum@users.noreply.github.com>
2025-03-11 23:16:26 -0400

    Update nested.ts
    creates new option array, either apppends or replaces the option based on targetOptionIndex, returns the updated question

--- src/nested.ts
+++ src/nested.ts
@@ -143,5 +143,17 @@ export function editOption(
     targetOptionIndex: number,
     newOption: string,
 ): Question[] {
-    return [];
+    return questions.map((question: Question): Question => {
+        if (question.id !== targetId) {
+            return { ...question };
+        }
+
+        const newOptions = [...question.options];
+        if (targetOptionIndex === -1) {
+            newOptions.push(newOption);
+        } else {
+            newOptions[targetOptionIndex] = newOption;
+        }
+        return { ...question, options: newOptions };
+    });
 }
