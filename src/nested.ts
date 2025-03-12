658dd24f6ad3b90ac2f4b1abfa16434a1c2ff5c3
0xR <114037840+0xRum@users.noreply.github.com>
2025-03-11 22:37:57 -0400

    mapping questions and isvalid fix.
    map questions

--- src/nested.ts
+++ src/nested.ts
@@ -65,7 +65,7 @@ export function getNames(questions: Question[]): string[] {
  * making the `text` an empty string, and using false for both `submitted` and `correct`.
  */
 export function makeAnswers(questions: Question[]): Answer[] {
-    return [];
+    return questions.map();
 }
 
 /***
