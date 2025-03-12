ee3184243142c814fcc7fbb1776c33d97e2d4885
0xR <114037840+0xRum@users.noreply.github.com>
2025-03-11 21:54:55 -0400

    Using map to transform the array of questions into an array of the names.

--- src/nested.ts
+++ src/nested.ts
@@ -56,7 +56,7 @@ export function removeQuestion(questions: Question[], id: number): Question[] {
  * Do not modify the input array.
  */
 export function getNames(questions: Question[]): string[] {
-    return [];
+    return questions.map((question: Question): string => question.name);
 }
 
 /**
