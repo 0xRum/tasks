b54ae8408fb7fe63dab3747a692f94e9cc7d715c
0xG <114037840+0xRum@users.noreply.github.com>
2025-04-08 23:49:55 -0400

    completed cycleholiday task

--- src/components/CycleHoliday.tsx
+++ src/components/CycleHoliday.tsx
@@ -1,6 +1,51 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
+import { act } from "@testing-library/react";
 
-export function CycleHoliday(): JSX.Element {
-    return <div>Cycle Holiday</div>;
+export function CycleHoliday(): React.JSX.Element {
+    const byAlphabet: Record<string, string> = {
+        "🐇": "🎆",
+        "🎆": "🎃",
+        "🎃": "🥂",
+        "🥂": "💖",
+        "💖": "🐇",
+    };
+
+    const byYear: Record<string, string> = {
+        "💖": "🐇",
+        "🐇": "🎆",
+        "🎆": "🎃",
+        "🎃": "🥂",
+        "🥂": "💖",
+    };
+
+    const [currentHoliday, changeCurrentHoliday] = useState<string>("💖");
+
+    return (
+        <>
+            <div>
+                <Button
+                    onClick={() => {
+                        act(() => {
+                            changeCurrentHoliday(byAlphabet[currentHoliday]);
+                        });
+                    }}
+                >
+                    Advance by Alphabet
+                </Button>
+                <Button
+                    onClick={() => {
+                        act(() => {
+                            changeCurrentHoliday(byYear[currentHoliday]);
+                        });
+                    }}
+                >
+                    Advance by Year
+                </Button>
+            </div>
+            <div>
+                <span>Holiday: {currentHoliday}</span>
+            </div>
+        </>
+    );
 }
