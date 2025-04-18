fd54a0a506c316ab3349666e068b3e91ad6f93ff
0xG <114037840+0xRum@users.noreply.github.com>
2025-04-18 01:17:13 -0400

    solved editmode

--- src/form-components/EditMode.tsx
+++ src/form-components/EditMode.tsx
@@ -1,9 +1,48 @@
 import React, { useState } from "react";
 
 export function EditMode(): React.JSX.Element {
+    const [isEditMode, setIsEditMode] = useState<boolean>(false);
+    const [name, setName] = useState<string>("Your Name");
+    const [isStudent, setIsStudent] = useState<boolean>(true);
+
     return (
         <div>
-            <h3>Edit Mode</h3>
+            <div className="form-switch">
+                <input
+                    type="checkbox"
+                    role="checkbox"
+                    checked={isEditMode}
+                    onChange={() => {
+                        setIsEditMode(!isEditMode);
+                    }}
+                />
+            </div>
+            {!isEditMode ?
+                <div>
+                    {name} is {isStudent ? "a student" : "not a student"}
+                </div>
+            :   <div>
+                    <input
+                        type="text"
+                        role="textbox"
+                        value={name}
+                        onChange={(e) => {
+                            setName(e.target.value);
+                        }}
+                    />
+                    <label>
+                        <input
+                            type="checkbox"
+                            role="checkbox"
+                            checked={isStudent}
+                            onChange={() => {
+                                setIsStudent(!isStudent);
+                            }}
+                        />
+                        Student
+                    </label>
+                </div>
+            }
         </div>
     );
 }
